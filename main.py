cd35cbbb304785b6effeac898677fd5d73a12ecc
WilsonVinson <cprewilsonvinson@gmail.com>
2023-03-21 18:10:20 +0800

    main

--- main.py
+++ main.py
@@ -19,10 +19,8 @@ print('            按下1 开始模仿            ')
 print('')
 
 while True:
-    # 检测按键并阻塞，直到按键被按下
     key = keyboard.read_event()
     
-    # 检查是否为按下事件
     if key.event_type == keyboard.KEY_DOWN:
         if key.name == 'f12':
             记录器 = listener.记录器()
