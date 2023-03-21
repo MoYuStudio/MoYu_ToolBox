b66c046ddbec2c91295e021439b204f5063ba8c6
WilsonVinson <cprewilsonvinson@gmail.com>
2023-03-21 16:05:59 +0800

    main

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 import sys
 
 import pyautogui
+import keyboard
 
-import json_driver
+import keyboard
 import listener
 
 json文件路径 = 'setting.json'
@@ -15,8 +16,18 @@ print('      Develop BY WilsonVinson       ')
 print('')
 print('           按下F12 开始学习           ')
 print('            按下1 开始模仿            ')
+print('')
 
-记录器 = listener.记录器()
-记录器.开始()
+while True:
+    # 检测按键并阻塞，直到按键被按下
+    key = keyboard.read_event()
     
+    # 检查是否为按下事件
+    if key.event_type == keyboard.KEY_DOWN:
+        if key.name == 'f12':
+            记录器 = listener.记录器()
+            记录器.开始()
+        if key.name == 'a':
+            print('run')
+
 sys.exit(0)
