74488bfe9c47cbe2d15b07e8690faf177744d650
WilsonVinson <cprewilsonvinson@gmail.com>
2023-03-22 17:35:37 +0800

    main

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import datetime
 import threading
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
+from tkinter import font
+from ttkbootstrap import Style
 
 import pyautogui
 
@@ -37,6 +40,9 @@ def start_execution(executor_obj, status_label_var):
 
 def clear_records(recorder_obj):
     recorder_obj.events.clear()
+    
+def on_closing():
+    root.destroy()
 
 if __name__ == '__main__':
     
@@ -51,40 +57,51 @@ if __name__ == '__main__':
         data = json_driver.json_read('data/user.json')
     executor_obj = executor.Executor(data)
     
-    root = tk.Tk()
+    # root = tk.Tk()
+    style = Style(theme='minty')# python -m ttkbootstrap
+    root = style.master
+    
     root.title('AutoClicker')
     # root.geometry("300x200")
+    
+    custom_font_0 = font.Font(family='黑体', size=12)#, weight='bold'
+    custom_font_1 = font.Font(family='黑体', size=7)
 
     screen_width, screen_height = pyautogui.size()
+    
+    title_label = Label(root, text='AutoClick 自动点击器', font=custom_font_0)
+    title_label.grid(row=0,column=0)
         
-    label1 = Label(root, text=('屏幕分辨率 '+str(screen_width)+'x'+str(screen_height)))
-    label1.grid(row=0,column=0)
+    label1 = Label(root, text=('屏幕分辨率 '+str(screen_width)+'x'+str(screen_height)), font=custom_font_0)
+    label1.grid(row=1,column=0)
     
     status_label_var = StringVar()
     status_label_var.set('就绪')
-    status_label = Label(root, textvariable=status_label_var, bd=1, relief=SUNKEN, anchor=W)
-    status_label.grid(row=0,column=1)
+    status_label = Label(root, textvariable=status_label_var, bd=1, relief=SUNKEN, anchor=W, font=custom_font_0)
+    status_label.grid(row=1,column=1)
     
-    file_name_label = Label(root, text='输入文件名（可选）:')
-    file_name_label.grid(row=1,column=0)
+    file_name_label = Label(root, text='输入文件名（可选）:', font=custom_font_0)
+    file_name_label.grid(row=2,column=0)
     
     file_name_entry = Entry(root, width=10)
-    file_name_entry.grid(row=1,column=1)
+    file_name_entry.grid(row=2,column=1)
 
-    record_button = tk.Button(root, text='开始记录', command=lambda: start_recording(recorder_obj, status_label_var))
-    record_button.grid(row=2,column=0)
+    record_button = tk.Button(root, text='开始记录', command=lambda: start_recording(recorder_obj, status_label_var), font=custom_font_0)
+    record_button.grid(row=3,column=0)
 
-    stop_button = tk.Button(root, text='结束记录', command=lambda: stop_recording(recorder_obj, status_label_var, file_name_entry))
-    stop_button.grid(row=2,column=1)
+    stop_button = tk.Button(root, text='结束记录', command=lambda: stop_recording(recorder_obj, status_label_var, file_name_entry), font=custom_font_0)
+    stop_button.grid(row=3,column=1)
 
-    execute_button = tk.Button(root, text='开始执行', command=lambda: start_execution(executor_obj, status_label_var))
-    execute_button.grid(row=3,column=0)
+    execute_button = tk.Button(root, text='开始执行', command=lambda: start_execution(executor_obj, status_label_var), font=custom_font_0)
+    execute_button.grid(row=4,column=0)
 
-    clear_button = tk.Button(root, text='清空记录', command=lambda: clear_records(recorder_obj))
-    clear_button.grid(row=3,column=1)
+    clear_button = tk.Button(root, text='清空记录', command=lambda: clear_records(recorder_obj), font=custom_font_0)
+    clear_button.grid(row=4,column=1)
     
-    copyright_label = Label(root, text='Develop BY WilsonVinson  ')
-    copyright_label.grid(row=4,column=0)
+    copyright_label = Label(root, text='Power BY ChatGPT   Develop BY WilsonVinson', font=custom_font_1)
+    copyright_label.grid(row=5,column=0)
+
+    root.protocol("WM_DELETE_WINDOW", on_closing)
 
     root.mainloop()
 
