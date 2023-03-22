ba0121ea0c83765b5ea9c817abda249ed375492b
WilsonVinson <cprewilsonvinson@gmail.com>
2023-03-22 12:00:44 +0800

    main

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import json_driver
 import listener
 import clicker
 
+
+
 设置配置路径 = 'setting.json'
 输入存档路径 = 'user.json'
 
@@ -19,18 +21,24 @@ print('           按下F12 开始学习           ')
 print('            按下a 开始模仿            ')
 print('')
 
+def on_hotkey_pressed():
+    print('Ctrl+C pressed')
+    keyboard.unhook_all()
+
 while True:
     键 = keyboard.read_event()
     
+    keyboard.add_hotkey('ctrl+c', on_hotkey_pressed)
+    
     if 键.event_type == keyboard.KEY_DOWN:
-        print(键.name)
-        if 键.name == 'f12': 
-            记录器 = listener.记录器()
-            记录器.运行()
-                  
-        if 键.name == 'a': 
-            数据 = json_driver.json文件读取(输入存档路径)
-            执行器 = clicker.执行器(数据)
-            执行器.运行()
+       print(键.name)
+       if 键.name == 'f12':
+           记录器 = listener.记录器()
+           记录器.运行()
+                
+       if 键.name == 'a': 
+           数据 = json_driver.json文件读取(输入存档路径)
+           执行器 = clicker.执行器(数据)
+           执行器.运行()
 
 sys.exit(0)
