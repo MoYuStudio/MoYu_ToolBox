58d184e7d3313c6bb38c60f34cb7fa25b8400fe2
WilsonVinson <cprewilsonvinson@gmail.com>
2023-03-22 20:02:59 +0800

    main

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from tkinter import font
 from ttkbootstrap import Style
 
 import pyautogui
+from PIL import Image, ImageTk
 
 import module.json_driver as json_driver
 import module.recorder as recorder
@@ -61,45 +62,67 @@ if __name__ == '__main__':
     style = Style(theme='minty')# python -m ttkbootstrap
     root = style.master
     
-    root.title('AutoClicker')
-    # root.geometry("300x200")
+    root.title('AutoClicker 自动点击器')
+    root.iconbitmap("icon.ico")
+    root.geometry("600x600")
+    root.resizable(False, False)
+    root.configure(bg='#F0F0F0')
+    root.attributes("-alpha", 0.95)
     
     custom_font_0 = font.Font(family='黑体', size=12)#, weight='bold'
-    custom_font_1 = font.Font(family='黑体', size=7)
+    custom_font_1 = font.Font(family='黑体', size=9)
+    custom_font_2 = font.Font(family='黑体', size=24)
+    
+    #button_style = ttk.Style()
+    #button_style.configure('Custom.TButton', font=custom_font_0, background='#80CBC4', foreground='#FFFFFF')
+    #button_style.map('Custom.TButton', background=[('active', '#4DB6AC'), ('pressed', '#009688')])
+
+    #button_frame = ttk.Frame(root)
+    #button_frame.grid(row=3, column=0, columnspan=2, pady=10)
 
     screen_width, screen_height = pyautogui.size()
     
-    title_label = Label(root, text='AutoClick 自动点击器', font=custom_font_0)
-    title_label.grid(row=0,column=0)
+    image_file = Image.open("icon_x500.png")
+    tk_image = ImageTk.PhotoImage(image_file)
+    icon_label = tk.Label(root, image=tk_image)
+    icon_label.place(x=5, y=5)
+    icon_label.config(bg=root['bg'])
+    
+    title_label = Label(root, text='AutoClick 自动点击器', font=custom_font_2)
+    title_label.place(x=95, y=25)
+    title_label.config(bg=root['bg'])
         
     label1 = Label(root, text=('屏幕分辨率 '+str(screen_width)+'x'+str(screen_height)), font=custom_font_0)
-    label1.grid(row=1,column=0)
+    label1.place(x=25, y=100)
+    label1.config(bg=root['bg'])
     
     status_label_var = StringVar()
     status_label_var.set('就绪')
     status_label = Label(root, textvariable=status_label_var, bd=1, relief=SUNKEN, anchor=W, font=custom_font_0)
-    status_label.grid(row=1,column=1)
+    status_label.place(x=500, y=100)
     
     file_name_label = Label(root, text='输入文件名（可选）:', font=custom_font_0)
-    file_name_label.grid(row=2,column=0)
+    file_name_label.place(x=25, y=150)
+    file_name_label.config(bg=root['bg'])
     
-    file_name_entry = Entry(root, width=10)
-    file_name_entry.grid(row=2,column=1)
+    file_name_entry = Entry(root, width=26)
+    file_name_entry.place(x=270, y=150)
 
-    record_button = tk.Button(root, text='开始记录', command=lambda: start_recording(recorder_obj, status_label_var), font=custom_font_0)
-    record_button.grid(row=3,column=0)
+    record_button = Button(root, text='开始记录',width=15, height=2, font=custom_font_0, command=lambda: start_recording(recorder_obj, status_label_var))
+    record_button.place(x=25, y=200)
 
-    stop_button = tk.Button(root, text='结束记录', command=lambda: stop_recording(recorder_obj, status_label_var, file_name_entry), font=custom_font_0)
-    stop_button.grid(row=3,column=1)
+    stop_button = Button(root, text='结束记录',width=15, height=2, font=custom_font_0, command=lambda: stop_recording(recorder_obj, status_label_var, file_name_entry))
+    stop_button.place(x=300, y=200)
 
-    execute_button = tk.Button(root, text='开始执行', command=lambda: start_execution(executor_obj, status_label_var), font=custom_font_0)
-    execute_button.grid(row=4,column=0)
+    execute_button = Button(root, text='开始执行',width=15, height=2, font=custom_font_0, command=lambda: start_execution(executor_obj, status_label_var))
+    execute_button.place(x=25, y=300)
 
-    clear_button = tk.Button(root, text='清空记录', command=lambda: clear_records(recorder_obj), font=custom_font_0)
-    clear_button.grid(row=4,column=1)
+    clear_button = Button(root, text='清空记录',width=15, height=2, font=custom_font_0, command=lambda: clear_records(recorder_obj))
+    clear_button.place(x=300, y=300)
     
     copyright_label = Label(root, text='Power BY ChatGPT   Develop BY WilsonVinson', font=custom_font_1)
-    copyright_label.grid(row=5,column=0)
+    copyright_label.place(x=100, y=560)
+    copyright_label.config(bg=root['bg'])
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
 
