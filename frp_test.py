68fa5e1f716da4fc0daaf17c7a969ce085bb8129
WilsonVinson <cprewilsonvinson@gmail.com>
2023-03-23 22:32:41 +0800

    main

--- frp_test.py
+++ frp_test.py
@@ -5,3 +5,10 @@ frpc_path = "frp/frpc"
 config_file = "frp/frpc.ini"
 
 process = subprocess.Popen([frpc_path, "-c", config_file])
+
+# # 停止frpc客户端
+# process.terminate()
+
+# # 重启frpc客户端
+# process.terminate()
+# process = subprocess.Popen([frpc_path, "-c", config_file])
