ce8a5244dc59ee1b5314aa6caf719e29eca2bd94
WilsonVinson <cprewilsonvinson@gmail.com>
2023-03-29 16:33:04 +0800

    main

--- module/bilibili_live.py
+++ module/bilibili_live.py
@@ -5,30 +5,72 @@ import json
 import requests
 import pyttsx3
 import threading
+from gtts import gTTS
+from io import BytesIO
+import pygame
 
 class BilibiliLive:
     def __init__(self, roomid=7193936, sleep_time=1):
         self.roomid = roomid
         self.sleep_time = sleep_time
         self.url = f"https://api.live.bilibili.com/xlive/web-room/v1/dM/gethistory?roomid={roomid}"
-        self.engine = pyttsx3.init()
-        self.engine.setProperty('voice', 1)
-        self.engine.setProperty('rate', 230)
-        self.engine.setProperty('volume', 0.8)
+        
         self.danmuhistory = []
+        self.gifts = []
         self.last_timestamp = ''
         self.stop_thread = False
+        
+        self.tts_engine = 'google' # [pyttsx3, google]
+        
+        self.pyttsx3_engine = pyttsx3.init()
+        self.pyttsx3_engine.setProperty('voice', 1)
+        self.pyttsx3_engine.setProperty('rate', 230)
+        self.pyttsx3_engine.setProperty('volume', 0.8)
+        
+        self.google_language = 'zh-CN' #'zh-CN' 'zh-TW'
+        self.google_slow_audio_speed = False
+        pygame.mixer.init()
+        
 
     def show(self, danmulist):
         for i in range(len(danmulist)):
             print('[{0}] {1} : {2}'.format(danmulist[i]['timeline'], danmulist[i]['nickname'], danmulist[i]['text']))
-            self.engine.say(danmulist[i]['nickname'] + '说：' + danmulist[i]['text'])
-            self.engine.runAndWait()
+            
+            if self.tts_engine ==  'pyttsx3':
+                self.pyttsx3_engine.say(danmulist[i]['nickname'] + '说：' + danmulist[i]['text'])
+                self.pyttsx3_engine.runAndWait()
+
+            if self.tts_engine == 'google':
+
+                google_tts_audio = gTTS(text=' '+danmulist[i]['nickname'] + '说：' + danmulist[i]['text'], lang=self.google_language, slow=self.google_slow_audio_speed)
+
+                audio_stream = BytesIO()
+                google_tts_audio.write_to_fp(audio_stream)
+                audio_stream.seek(0)
+
+                # 加载音频流
+                pygame.mixer.music.load(audio_stream)
+
+                # 播放音频文件
+                pygame.mixer.music.play()
+
+                # 等待音频播放结束
+                while pygame.mixer.music.get_busy():
+                    pygame.time.Clock().tick(10)
 
     def fetch_danmu(self):
         r = requests.get(self.url)
         message = json.loads(r.text)
         return message['data']['room']
+    
+    def fetch_gifts(self):
+        url = f"https://api.live.bilibili.com/gift/v2/live/receive_record?roomid={self.roomid}&page=1"
+        r = requests.get(url)
+        data = r.json()
+        if data["code"] == 0:
+            return data["data"]["list"]
+        else:
+            return []
 
     def run(self):
         initial_danmu = self.fetch_danmu()
@@ -56,6 +98,14 @@ class BilibiliLive:
                 self.last_timestamp = new_danmaku_list[-1]['timeline']
                 self.danmuhistory += new_danmaku_list
                 self.show(new_danmaku_list)
+                
+            new_gifts = self.fetch_gifts()
+            for gift in new_gifts:
+                if gift not in self.gifts:
+                    self.gifts.append(gift)
+                    thank_msg = f"{gift['uname']} 赠送了 {gift['num']} 个 {gift['gift_name']}，感谢支持！"
+                    self.engine.say(thank_msg)
+                    self.engine.runAndWait()
 
             time.sleep(self.sleep_time)
 
--- tts_google_test.py
+++ tts_google_test.py
@@ -1,28 +1,27 @@
-
+from io import BytesIO
 from gtts import gTTS
 import pygame
 
 # 定义要转换为语音的文本
-text = "Hello,早上好"
+text = "0 Hello,早上好"
 
 # 设置语言和其他参数
 language = 'zh-CN' #'zh-CN' 'zh-TW'
 slow_audio_speed = False
 
 # 创建一个 gTTS 对象
-my_audio = gTTS(text=text, lang=language, slow=slow_audio_speed)
+my_audio = gTTS(text=text, lang=language, slow=1.5)
 
-# 保存生成的音频文件
-my_audio.save("output_audio.mp3")
-
-# 播放生成的音频文件（仅限于 macOS 和 Linux，Windows 用户需要使用其他方法）
-# os.system("mpg321 output_audio.mp3")
+# 生成音频流
+audio_stream = BytesIO()
+my_audio.write_to_fp(audio_stream)
+audio_stream.seek(0)
 
 # 初始化 pygame
 pygame.mixer.init()
 
-# 加载音频文件
-pygame.mixer.music.load("output_audio.mp3")
+# 加载音频流
+pygame.mixer.music.load(audio_stream)
 
 # 播放音频文件
 pygame.mixer.music.play()
