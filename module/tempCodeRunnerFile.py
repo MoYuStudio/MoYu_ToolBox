64f6089af2a34c34701aa6b78c9aa067442a0827
WilsonVinson <cprewilsonvinson@gmail.com>
2023-09-05 11:33:46 +0800

    main

---
+++ module/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+ # ws.close()
+        # ws_thread.join()
+
+        # self.pyttsx3_engine.stop()
--- test/stock/stock.py
+++ test/stock/stock.py
@@ -10,7 +10,7 @@ all_data = pd.DataFrame()
 
 for code in codes:
     # 获取基金数据
-    data = yf.download(code, start='2023-01-01', end='2023-04-25')
+    data = yf.download(code, start='2023-01-01', end='2023-05-1')
 
     # 计算移动平均线
     data['SMA_5'] = data['Close'].rolling(window=5).mean()
